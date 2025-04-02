6b19346a64ce7ddf1719396693466ffe6ca46aea
Tomi Shittu <102392057+t0miXhi-II@users.noreply.github.com>
2025-04-02 19:46:29 +1100

    completed version 1
    note-to-self => fix save_tool in tools.py  (currently not saving output to text)

--- main.py
+++ main.py
@@ -5,10 +5,77 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-
+import anthropic
+from langchain_core.prompts import ChatPromptTemplate # For prompt template
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 
 load_dotenv() # Loads the .env file
 
-if __name__=="__main__":
-    print("True")
+class ResearchResponse(BaseModel): # BaseModel is from the 'pydantic' library
+    '''This class specifies the type of content that 
+        we want our LLM to generate or create.
+        This is the format for the output.'''
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+    '''The information above specifies all the fields which 
+        will serve as output format from our LLM.'''
+    
+
+# LLM Choice - There are 2 options, as shown below
+llm  = ChatAnthropic(model="claude-3-5-sonnet-20241022") # Don't forget your API key => Get Anthropic API key from: https://console.anthropic.com/settings/keys
+# llm_option2  = ChatOpenAI(model="gpt-4o-mini") # if this is chosen, a ChatOpenAI API Key will be needed => get OpenAI API key from: https://platform.openai.com/api-keys
+
+
+# Output Response Parser
+parser = PydanticOutputParser(pydantic_object=ResearchResponse) # ResearchResponse is the template class specified above
+
+# AI Agent prompt - instructions/guide for our AI Agent
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper. 
+            Answer the user query and use necessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"), # Note the quary part
+        ("placeholder", "{agent_scratchpad}")
+    ]
+).partial(format_instructions=parser.get_format_instructions()) # Our output template from the 'parser' variable is fed as input into the prompt
+# The format for the prompt above can be found in the langchain documentation
+
+
+# Agent creation
+tools = [search_tool, wiki_tool, save_tool] # Tools from tools.py (agent can still work without them)
+agent = create_tool_calling_agent(
+    llm = llm,
+    prompt = prompt,
+    tools = tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True) # Verbose=True -> shows the thinking process of the AI Agent; the value can be set to False to turn it Off.
+query = input("Hi, What would you like to know? \n")
+raw_response = agent_executor.invoke({"query": query}) # Query from the prompt section above; You can pass muiltiple queries -> e.g. {"query": "What is the capital of Italy?", "name": "Alice"}
+
+# Customising the Agent response presentation
+try: 
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response")
+    print("Error: ", e)
+    print("Raw Response: ", raw_response)
+
+
+
+#response = llm.invoke("What is the meaning of invoke?")
+#print(f"LLM Response: \n{response}")
+
--- tools.py
+++ tools.py
@@ -1,1 +1,38 @@
 # This file for for storing the tools the AI Agent will be using
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
+
+
+# Custom Tool - The tool saves the 
+def save_to_txt(data: str, filename: str = "research_output.txt"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
+
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(formatted_text)
+
+    return f"Data successfully saved to {filename}"
+
+
+# Wrap/package our above function as a tool -> name: save_tool
+save_tool = Tool(   
+    name = "save_text_to_file", # Any name can be used here (name MUST NOT have any spaces)
+    func = save_to_txt, # The function the tool will be providing
+    description = "Save structured research data to a text file",
+) 
+
+# DuckDuckGoSearchRun Tool -> name: search_tool
+search = DuckDuckGoSearchRun()
+search_tool = Tool(
+    name = "search", # Any name can be used here (name MUST NOT have any spaces)
+    func = search.run, # The function the tool will be providing
+    description = "Search the web for information",
+)
+
+# Wikipedia Tool -> name: wiki_tool
+api_wrapper = WikipediaAPIWrapper(top_k_results = 1, doc_content_chars_max = 120)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+
